ec7f572c3c40da9cb1736a59fd22818d9a397f2d
Dirk Meyer <dischi@freevo.org>
2005-11-13 18:15:09 +0000

    create freevo core distribution module
    git-svn-id: svn://svn.freevo.org/freevo/trunk/tvserver@7783 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- setup.py
+++ setup.py
@@ -1,21 +1,43 @@
-# Python distutils stuff
-import os
-import sys
-from distutils.core import setup, Extension
+# -*- coding: iso-8859-1 -*-
+# -----------------------------------------------------------------------------
+# setup.py - setup script for installing the freevo module
+# -----------------------------------------------------------------------------
+# $Id: mcomm.py 7704 2005-10-15 18:48:13Z dmeyer $
+#
+# -----------------------------------------------------------------------------
+# Freevo - A Home Theater PC framework
+# Copyright (C) 2002-2005 Krister Lagerstrom, Dirk Meyer, et al.
+#
+# First Edition: Dirk Meyer <dmeyer@tzi.de>
+# Maintainer:    Dirk Meyer <dmeyer@tzi.de>
+#
+# Please see the file doc/CREDITS for a complete list of authors.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MER-
+# CHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+# -----------------------------------------------------------------------------
 
-share_files = [( 'share/freevo/config/', ['share/config/tvserver.conf'])]
+from freevo.distribution import setup
 
 # now start the python magic
-setup (name         = "freevo",
-       version      = "2.0",
-       description  = "Freevo",
-       author       = "Krister Lagerstrom, et al.",
-       author_email = "freevo-devel@lists.sourceforge.net",
-       url          = "http://www.freevo.org",
-       license      = "GPL",
-
-       package_dir = { 'freevo.tvserver': 'src' },
-       packages    = [ 'freevo.tvserver', 'freevo.tvserver' ],
-       scripts     = [ 'bin/freevo-tvserver' ],
-       data_files  = share_files
+setup (name         = 'freevo-tvserver',
+       module       = 'tvserver',
+       version      = '2.0',
+       description  = 'Freevo TV Server',
+       author       = 'Dirk Meyer, et al.',
+       author_email = 'freevo-devel@lists.sourceforge.net',
+       url          = 'http://www.freevo.org',
+       license      = 'GPL',
        )
